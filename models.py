1995c214e8026a71ffb678cf0317e5e3b1f403a9
Sean Hanlon <sean.a.hanlon@gmail.com>
2023-08-03 06:38:04 +0100

    Removed commented out  code from models

--- models.py
+++ models.py
@@ -5,7 +5,6 @@ class Athlete(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
-    # exercises = db.relationship('exercises', backref='athletes')
 
     def __repr__(self):
         return f"<Name: {self.id}: {self.name}>"
@@ -27,20 +26,3 @@ def remove_exercise(id):
     exercise = Exercise.query.get(id)
     db.session.delete(exercise)
     db.session.commit()
-
-# class Exercise_categories(db.Model):
-#     __tablename__ = "exercise_categories"
-#     id = db.Column(db.Integer, primary_key=True)
-#     exercise_id = db.relationship(db.Integer, db.ForeignKey('exercises.id'))
-#     category_id = db.relationship(db.Integer, db.ForeignKey('categories.id'))
-
-    # def __repr__(self):
-    #     return f"<Exercise_categories: {self.id}>"
-
-# class Categories(db.Model):
-#     __tablename__ = "categories"
-#     id = db.Column(db.Integer, primary_key=True, nullable=False)
-#     name = db.Column(db.String(64), required=True)
-
-#     def __repr__(self):
-#         return f"<Name: {self.id}: {self.name}>"
